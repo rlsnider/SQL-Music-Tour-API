af1682f810a7abbef8932a0fc3e50f2ca44f49e6
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-11-12 12:57:54 -0800

    added 3 models with on fkeys

---
+++ migrations/20221112204156-create-event.js
@@ -0,0 +1,22 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('events', {
+      event_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      
+      event_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('events');
+  }
+};
